206e441b9ec6c3cd508814459066c06392df13d1
Lynn <code@krogseng.com>
2017-01-07 09:24:54 -0800

    changed the ajax call to call my repository.

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -4,17 +4,10 @@
   reposObj.allRepos = [];
 
   reposObj.requestRepos = function(callback) {
-    $.ajax('https://api.github.com/user/repos', {
-      method: 'GET', // usually default
-      //headers: {Authorization: 'token ' + githubToken}, // this or parameters version
-      data: {access_token: githubToken}, // parameters version, developer.github.com/v3/
-      success: function(response) {
-        reposObj.allRepos = response;
-        callback();
-      },
-      error: function(response) {
-      }
-    });
+    $.get('/github/users/krogseng/repos')
+        .done(function(data) {
+          reposObj.allRepos = data;
+        }).done(callback);
   };
 
   reposObj.withTheAttribute = function(myAttr) {
@@ -23,6 +16,5 @@
     });
   };
 
-  //reposObj.requestRepos();
   module.reposObj = reposObj;
 })(window);
